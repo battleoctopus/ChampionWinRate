55c5f3b7b877b1f2ee14a5a65daa81291f06a458
George Yu <georgelyu@gmail.com>
2014-09-20 13:48:23 -0500

    Changed Reader to only use a single API key instead of multiple keys.

--- ChampionWinRate/Reader.cs
+++ ChampionWinRate/Reader.cs
@@ -10,37 +10,20 @@ using System.Threading.Tasks;
 namespace ChampionWinRate
 {
     // Takes in a string URL request without the API key and returns the JSON
-    // object as a string. A dictionary maps API keys to queues of their ten
-    // most recent requests.
+    // object as a string.
     class Reader
     {
-        private Dictionary<String, Queue<DateTime>> keysDict = new Dictionary<string,Queue<DateTime>>();
-        private const String DAWN_OF_TIME = "0001-01-01T00:00:00.000"; // arbitrary time long before any requests
-        private const int MAX_REQUESTS_PER_INTERVAL = 10;
-        private const double INTERVAL = 10; // measured in seconds
+        private String key;
         private const String RATE_LIMIT_CODE = "429";
         private const String RATE_LIMIT_EXCEEDED = "rate limit exceeded";
 
         public Reader()
         {
-            String[] keys = Resources.ApiKeys.Split(','); // API keys are stored as a resource
-
-            foreach (String key in keys)
-            {
-                Queue<DateTime> timeQueue = new Queue<DateTime>();
-                
-                // instantiate each queue of request times
-                for (int i = 0; i < MAX_REQUESTS_PER_INTERVAL; i++)
-                {
-                    timeQueue.Enqueue(DateTime.Parse(DAWN_OF_TIME));
-                }
-
-                keysDict[key] = timeQueue;
-            }
+            this.key = Resources.ApiKeys;
         }
 
         // Sends an API request to riotgames.com. Returns an empty string if
-        // Riot doesn't honor its rate limit. API key must be included in url.
+        // Riot returns an error. API key must be included in url.
         private String Request(String url)
         {
             WebClient client = new WebClient();
@@ -69,39 +52,28 @@ namespace ChampionWinRate
             }
         }
 
-        // Sends an API request for static data to riotgames.com. Adds the first
-        // API key since static requests do not count towards the rate limit.
+        // Sends an API request for static data to riotgames.com. Static
+        // requests do not count towards the rate limit.
         public String RequestStatic(String url)
         {
-            String message = Request(url + keysDict.Keys.First());
-            return message;
+            return Request(url + key);
         }
 
-        // Tries to send an API request to riotgames.com. If all API keys have
-        // reached their rate limit, the method stalls.
+        // Tries to send an API request to riotgames.com, stalling if the rate
+        // limit is reached.
         public String TryRequest(String url)
         {
-            // stall until an API key is under the rate limit
+            // stall until the API key is under the rate limit
             while (true)
             {
-                foreach (String key in keysDict.Keys)
+                String message = Request(url + key);
+
+                if (message == RATE_LIMIT_EXCEEDED)
                 {
-                    Queue<DateTime> timeQueue = keysDict[key];
-
-                    if (DateTime.Now - timeQueue.Peek() > TimeSpan.FromSeconds(INTERVAL))
-                    {
-                        String message = Request(url + key);
-
-                        if (message == RATE_LIMIT_EXCEEDED)
-                        {
-                            break;
-                        }
-
-                        timeQueue.Enqueue(DateTime.Now);
-                        timeQueue.Dequeue();
-                        return message;
-                    }
+                    continue;
                 }
+
+                return message;
             }
         }
     }
